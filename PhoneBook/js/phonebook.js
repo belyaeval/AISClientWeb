e565631b6181cca1a959cdcc7f96eaa5d28a75f4
belyaeval <belen@yandex.ru>
2020-01-22 00:26:43 +0700

    Задача PhoneBook jQuery

--- PhoneBook/js/phonebook.js
+++ PhoneBook/js/phonebook.js
@@ -9,62 +9,63 @@ $(document).ready(function () {
     var i = 0;
 
     addButton.click(function () {
-            if (surname.val() === "" || name.val() === "" || phoneNumber.val() === "") {
-                $(inputData).addClass("validation-help");
-                $(validationMessage).text("Не заполнены обязательные поля");
-                validationMessage.show();
+        if (surname.val() === "" || name.val() === "" || phoneNumber.val() === "") {
+            $(inputData).addClass("validation-help");
+            $(validationMessage).text("Не заполнены обязательные поля");
+            validationMessage.show();
 
-                if (surname.val() !== "") {
-                    $(surname).removeClass("validation-help");
-                }
-
-                if (name.val() !== "") {
-                    $(name).removeClass("validation-help");
-                }
-
-                if (phoneNumber.val() !== "") {
-                    $(phoneNumber).removeClass("validation-help");
-                }
-
-                return;
+            if (surname.val() !== "") {
+                $(surname).removeClass("validation-help");
             }
 
-            if (isNaN(phoneNumber.val())) {
-                $(phoneNumber).addClass("validation-help");
-                $(validationMessage).text("Неверно заполен номер телефона");
-                validationMessage.show();
-                return;
+            if (name.val() !== "") {
+                $(name).removeClass("validation-help");
             }
 
-            validationMessage.hide();
-            $(phoneNumber).removeClass("validation-help");
+            if (phoneNumber.val() !== "") {
+                $(phoneNumber).removeClass("validation-help");
+            }
 
-            i++;
-
-            var newRow = $("<tr>")
-                .append($("<td>").text(i))
-                .append($("<td>").text(surname.val()))
-                .append($("<td>").text(name.val()))
-                .append($("<td>").text(phoneNumber.val()))
-                .append($("<td>")
-                    .append($("<button type='button'>X</button>"))
-                    .click(function () {
-                        newRow.remove();
-
-                        $("tr:has(td)").each(function (index) {
-                            var currentNumber = $(this).children().eq(0);
-                            var currentNumberValue = Number(currentNumber.text()) - 1;
-
-                            if (index < currentNumberValue) {
-                                currentNumber.text(currentNumberValue);
-                            }
-                        });
-                        i--;
-                    })
-                );
-
-            $(inputData).val("");
-            table.append(newRow);
+            return;
         }
-    );
+
+        if (isNaN(phoneNumber.val())) {
+            $(phoneNumber).addClass("validation-help");
+            $(validationMessage).text("Неверно заполен номер телефона");
+            validationMessage.show();
+            return;
+        }
+
+        validationMessage.hide();
+        $(surname).removeClass("validation-help");
+        $(name).removeClass("validation-help");
+        $(phoneNumber).removeClass("validation-help");
+
+        i++;
+
+        var newRow = $("<tr>")
+            .append($("<td>").text(i))
+            .append($("<td>").text(surname.val()))
+            .append($("<td>").text(name.val()))
+            .append($("<td>").text(phoneNumber.val()))
+            .append($("<td>")
+                .append($("<button type='button'>X</button>"))
+                .click(function () {
+                    newRow.remove();
+
+                    $("tr:has(td)").each(function (index) {
+                        var currentNumber = $(this).children().eq(0);
+                        var currentNumberValue = Number(currentNumber.text()) - 1;
+
+                        if (index < currentNumberValue) {
+                            currentNumber.text(currentNumberValue);
+                        }
+                    });
+                    i--;
+                })
+            );
+
+        $(inputData).val("");
+        table.append(newRow);
+    });
 });
