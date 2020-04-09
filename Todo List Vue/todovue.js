1f131b20b11ed0e77c8bed07bc02e3497a3545e0
belyaeval <belen@yandex.ru>
2020-04-10 03:35:06 +0700

    Задача Todo List Vue

--- Todo List Vue/todovue.js
+++ Todo List Vue/todovue.js
@@ -4,7 +4,6 @@ new Vue({
         items: [],
         newTodoText: "",
         isInvalid: false,
-        editTodoText: "",
         isEdit: false,
         itemId: 1
     },
@@ -29,25 +28,26 @@ new Vue({
 
         editTodo: function (item) {
             item.isEdit = true;
-            this.editTodoText = item.text;
+            item.newTodoText = item.text;
         },
 
         saveTodo: function (item) {
             item.isEdit = false;
 
-            if (this.editTodoText === "") {
+            if (item.text === "") {
                 item.isInvalid = true;
                 item.isEdit = true;
                 return;
             }
 
             item.isInvalid = false;
-            item.text = this.editTodoText;
+            item.newTodoText = item.text;
         },
 
         cancelTodo: function (item) {
             item.isEdit = false;
             item.isInvalid = false;
+            item.text = item.newTodoText;
         },
 
         deleteTodo: function (item) {
